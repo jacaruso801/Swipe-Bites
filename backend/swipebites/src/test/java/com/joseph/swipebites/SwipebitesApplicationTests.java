fa50cca19912374b11e8af574d22a30225268420
Joseph Caruso <61638075+jacaruso801@users.noreply.github.com>
2026-07-24 17:47:47 +0000

    feat: configure PostgreSQL with Docker Compose

---
+++ backend/swipebites/src/test/java/com/joseph/swipebites/SwipebitesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joseph.swipebites;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwipebitesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
